4fd0af0605fc6cd77d9ec9406503fda47ad19470
Akram Sabbah <akramsabbah9@gmail.com>
2021-02-03 02:57:07 -0800

    finished inquiry stuff

--- app.js
+++ app.js
@@ -33,9 +33,23 @@ const promptUser = () => {
             }
         },
         {
+            type: "confirm",
+            name: "confirmAbout",
+            message: "Would you like to enter some information about yourself for an \"About\" section?",
+            default: true
+        },
+        {
             type: "input",
             name: "about",
-            message: "Provide some information about yourself:"
+            message: "Provide some information about yourself:",
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
         }
     ]);
 };
@@ -98,6 +112,7 @@ Add a New Project
                     console.log("Please enter the link to the project!");
                     return false;
                 }
+            }
         },
         {
             type: "confirm",
